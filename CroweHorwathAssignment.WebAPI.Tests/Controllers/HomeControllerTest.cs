3e6c64d3185a2cc5895c2d5504ce883e0a882234
canadiantina <canadiantinamarieish@gmail.com>
2016-08-31 11:50:42 -0400

    first commit

---
+++ CroweHorwathAssignment.WebAPI.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CroweHorwathAssignment.WebAPI;
+using CroweHorwathAssignment.WebAPI.Controllers;
+
+namespace CroweHorwathAssignment.WebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ CroweHorwathAssignment.WebAPI.Tests/Controllers/MessageControllerTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CroweHorwathAssignment.WebAPI.Controllers;
+using System.Collections.Generic;
+using CroweHorwathAssignment.Service.Services;
+using CroweHorwathAssignment.Service.Interfaces;
+using CroweHorwathAssignment.Data.Models.DTO;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Http.Results;
+using CroweHorwathAssignment.Data.Core.Mapping;
+
+namespace CroweHorwathAssignment.WebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class MessageControllerTest
+    {
+
+        [TestMethod]
+        public void GetDBMessageWeb()
+        {
+            // Arrange
+            InitializeMapping();
+            var controller = new MessageController();
+
+            //Act
+            var actionResult = controller.GetMessage(1);
+
+            // Assert
+            var response = actionResult as OkNegotiatedContentResult<tblTagDTO>;
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual("Hello World Web DB", response.Content.tagDescription);
+        }
+        [TestMethod]
+        public void GetDBMessageConsole()
+        {
+            // Arrange
+            InitializeMapping();
+            var controller = new MessageController();
+
+            //Act
+            var actionResult = controller.GetMessage(2);
+
+            // Assert
+            var response = actionResult as OkNegotiatedContentResult<tblTagDTO>;
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual("Hello World Console DB", response.Content.tagDescription);
+        }
+        [TestMethod]
+        public void GetAllMessages()
+        {
+            // Arrange
+            InitializeMapping();
+            var controller = new MessageController();
+
+            //Act
+            IEnumerable<tblTagDTO> actionResult = controller.GetMessages();
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+        }
+        [TestMethod]
+        public void GetDefaultMessage()
+        {
+            // Arrange
+            InitializeMapping();
+            var controller = new MessageController();
+
+            //Act
+            tblTagDTO actionResult = controller.GetDefault();
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.AreEqual("Hello World", actionResult.tagDescription);
+        }
+
+        private static void InitializeMapping()
+        {
+            AutoMapper.Mapper.Initialize(config =>
+            {
+                config.AddProfile<CroweHorwathAssigmentDataMappingProfile>();
+            });
+            AutoMapper.Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
