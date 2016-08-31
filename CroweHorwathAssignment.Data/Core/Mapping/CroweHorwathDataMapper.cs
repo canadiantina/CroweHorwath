a50885bf96efc7e31af0978d18fac2d967ddcb7e
canadiantina <canadiantinamarieish@gmail.com>
2016-08-31 11:42:20 -0400

    initial commit

---
+++ CroweHorwathAssignment.Data/Core/Mapping/CroweHorwathDataMapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CroweHorwathAssignment.Data.Models;
+using CroweHorwathAssignment.Data.Models.DTO;
+using AutoMapper;
+
+namespace CroweHorwathAssignment.Data.Core.Mapping
+{
+    public class CroweHorwathAssigmentDataMappingProfile : AutoMapper.Profile
+    {
+        protected override void Configure()
+        {
+
+            try
+            {
+                base.Configure();
+
+                #region "DTO to Entity"
+
+                AutoMapper.Mapper.CreateMap<tblTagDTO, tblTag>();
+
+                #endregion
+
+                #region "Entity to DTO"
+
+                AutoMapper.Mapper.CreateMap<tblTag, tblTagDTO>();
+
+                #endregion
+
+                AutoMapper.Mapper.AssertConfigurationIsValid();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
