e22541f59c76a7e3596ec333db8a2fc6f43755a4
canadiantina <canadiantinamarieish@gmail.com>
2016-08-31 12:02:55 -0400

    initial commit

---
+++ CroweHorwathAssignment.WebAPI/App_Start/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CroweHorwathAssignment.Data.Core.Mapping;
+namespace CroweHorwathAssignment.WebAPI.App_Start
+{
+    public class MapperConfig
+    {
+        public static void ConfigureMapping()
+        {
+            AutoMapper.Mapper.Initialize(config =>
+            {
+                config.AddProfile<CroweHorwathAssigmentDataMappingProfile>();
+            });
+
+            AutoMapper.Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ CroweHorwathAssignment.WebAPI/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CroweHorwathAssignment.Data.Models.DTO;
+using CroweHorwathAssignment.Service.Services;
+using CroweHorwathAssignment.Service.Interfaces;
+
+namespace CroweHorwathAssignment.WebAPI.Controllers
+{
+    public class MessageController : ApiController
+    {
+        private IMessageService messageService = new MessageService();
+
+        public IEnumerable<tblTagDTO> GetMessages()
+        {
+            return messageService.GetMessages();
+        }
+
+        public IHttpActionResult GetMessage(int id)
+        {
+            tblTagDTO tag = messageService.GetMessage(id);
+            if (tag == null)
+                return NotFound();
+            return Ok(tag);
+        }
+
+        public tblTagDTO GetDefault()
+        {
+            return messageService.DefaultMessage();
+        }
+
+    }
+}
